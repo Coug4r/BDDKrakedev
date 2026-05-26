0b36ae7f0e157d2a7b31c704a7274af868288141
Coug4r <burneofrancisco2005@gmail.com>
2026-05-25 21:12:57 -0500

    Add: Utilidada ConexionBase de datos

---
+++ producto/producto/src/main/java/com/krakedev/jdbc/ConexionPostgres.java
@@ -0,0 +1,29 @@
+package com.krakedev.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.postgresql.util.PSQLException;
+
+public class ConexionPostgres {
+	private static final Logger log = LogManager.getLogger(ConexionPostgres.class);
+	public static Connection conexion(){
+		Connection con = null; 
+		try {
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BDDKrake","postgres","DJI_SPARK");
+			log.info("Conexion exitosa");
+			return con;
+		
+		} catch (PSQLException ec) {
+			log.error("Contraseña incorrecta!");
+			throw new RuntimeException("Contraseña incorrecta!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			log.error("Fallo en la conexion!");
+			throw new RuntimeException("Fallo en la conexion!");
+		}
+	}
+}
